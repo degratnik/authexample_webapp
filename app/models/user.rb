689a60da7a68c37f4ac58ea463ddb3af376dd585
Oleksandr Reshetnik <Oleksandr_Reshetnik@epam.com>
2013-07-05 17:35:15 +0300

    login logout

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :token_authenticatable
+  before_save :ensure_authentication_token
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthexampleWebapp::Application.config.secret_token = 'eaf1cfa610cd6a8cf37b454bccda8438fed981f14841724c4c33d927bfecb79589a6dfdf19e6dfe65a2fdaa5275da6560c217737854f9610b1f2f4efa086a6a6'
