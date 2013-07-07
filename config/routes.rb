2d0c955d25b60c641d4b1a0da8c8459fb3419251
Oleksandr Reshetnik <Oleksandr_Reshetnik@epam.com>
2013-07-07 11:08:51 +0300

    added register method

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ AuthexampleWebapp::Application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_scope :user do
+        post 'registrations' => 'registrations#create', :as => 'register'
         post 'sessions' => 'sessions#create', :as => 'login'
         delete 'sessions' => 'sessions#destroy', :as => 'logout'
       end
